d346fc407b3b67686c3ee923e4ee10641cf543e4
kellymarieod <kellymarieod@gmail.com>
2016-04-06 20:24:19 -0400

    JQUERY successfully tested

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-alert("IT'S WORKING");
+// alert ("It's working!")
+// ^ test for basic javascript connect to html
+
+$(document).ready( function() {
+  alert("Ca marche!");
+});
+// ^ test for JQUERY connect to html ("When the document is ready, do this=function=send me an alert saying Ca march!")
