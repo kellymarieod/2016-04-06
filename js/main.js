a67fe28fa60882437e83ec36c542e56a6e7f814e
kellymarieod <kellymarieod@gmail.com>
2016-04-06 20:52:50 -0400

    meow button makes small cat

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 // alert ("It's working!")
 // ^ test for basic javascript connect to html
 
-$(document).ready( function() {
-  alert("Ca marche!");
-});
+// $(document).ready( function() {
+//   alert("Ca marche!");
+// });
 // ^ test for JQUERY connect to html ("When the document is ready, do this=function=send me an alert saying Ca march!")
+
+$(document).ready( function() {
+  $("#meow").click( function() {
+    $("#animals").append("  <img src='images/cat.jpg' alt='cat in a roll' class='animal' />");
+  });
+});
